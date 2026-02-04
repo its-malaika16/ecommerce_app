891b6fe39f96651ae40893e0753d71dd370e9083
Malaika Zafar <malaikazafar696@gmail.com>
2026-02-04 06:04:09 +0400

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce_store/screens/auth-ui/splash-screen.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'screens/user-panel/main-screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return GetMaterialApp(
+home: const SplashScreen(),
     );
   }
 }
---
+++ lib/screens/auth-ui/splash-screen.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:ecommerce_store/screens/user-panel/main-screen.dart';
+import 'package:ecommerce_store/utils/app-constant.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Get.offAll(() => MainScreen());
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: AppConstant.appSecondaryColor,
+      appBar: AppBar(
+        backgroundColor: AppConstant.appSecondaryColor,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: Get.width,
+                alignment: Alignment.center,
+                child: Lottie.asset('assets/images/splash-icon.json'),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              width: Get.width,
+              alignment: Alignment.center,
+              child: Text(
+                AppConstant.appPoweredBy,
+                style: TextStyle(
+                  color: AppConstant.appTextColor,
+                  fontSize: 12,
+                ),
+              ),
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
